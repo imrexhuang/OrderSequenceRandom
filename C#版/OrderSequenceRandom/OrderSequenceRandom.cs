aca21ff990220007c6a48eb2439fb911ad827f30
Rex <im@rex.tw>
2019-09-29 22:40:25 +0800

    調整架構

--- C#版/OrderSequenceRandom/OrderSequenceRandom.cs
+++ C#版/OrderSequenceRandom/OrderSequenceRandom.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrderSequenceRandom
 {
-    class OrderSequenceRandom : AbstractOrderSequenceRandom
+    class OrderSequenceRandom
     {
+        //private static readonly String ORDERPREFIX = "SVSP";
+        private static readonly String ORDERPREFIX = "";
+
+
+        public static readonly String numberChar = "0123456789";
+
+        public static readonly int numberFor = 12;
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public String createOrderSnRandom()
+        {
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < numberFor; i++)
+            {
+                //Java寫法 //sb.append(numberChar.charAt( random.nextInt(numberChar.length()) ));
+                sb.Append(numberChar[random.Next(9)]); //C#寫法
+            }
+            //Java寫法 //return ORDERPREFIX + sdf.format(new Date()) + sb.toString();
+
+            String rtnString = ORDERPREFIX + DateTime.Now.ToString("yyyyMMdd") + sb.ToString();
+            Console.WriteLine(rtnString);
+            return rtnString;
+        }
 
     }
 }
--- C#版/OrderSequenceRandom/Program.cs
+++ C#版/OrderSequenceRandom/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OrderSequenceRandom
@@ -11,8 +12,35 @@ namespace OrderSequenceRandom
         static void Main(string[] args)
         {
             OrderSequenceRandom osr = new OrderSequenceRandom();
-            Console.WriteLine(OrderSequenceRandom.createOrderSnRandom());
+            osr.createOrderSnRandom();
+            osr.createOrderSnRandom();
+
+            Thread thread1 = new Thread(Function1);
+            Thread thread2 = new Thread(Function2);
+            Thread thread3 = new Thread(Function3);
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
             Console.ReadLine();
         }
+
+        static void Function1()
+        {
+            OrderSequenceRandom osr1 = new OrderSequenceRandom();
+            osr1.createOrderSnRandom();
+        }
+
+        static void Function2()
+        {
+            OrderSequenceRandom osr2 = new OrderSequenceRandom();
+            osr2.createOrderSnRandom();
+        }
+
+        static void Function3()
+        {
+            OrderSequenceRandom osr3 = new OrderSequenceRandom();
+            osr3.createOrderSnRandom();
+        }
+
     }
 }
