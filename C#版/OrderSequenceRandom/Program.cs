20c9763a6caba04b39fd92a2b043dca5cfec1342
李進興 Vulcan <vulcan@miniasp.com>
2019-09-30 01:46:37 +0800

    測試每個執行緒都產生一個 Random 物件與只有單一 Random 物件的效果

--- C#版/OrderSequenceRandom/Program.cs
+++ C#版/OrderSequenceRandom/Program.cs
@@ -24,6 +24,34 @@ namespace OrderSequenceRandom
                     osr1.createOrderSnRandom();
                 }); ;
             }
+
+            Thread.Sleep(1000);
+            Console.WriteLine(new String('=', 80));
+
+            // 這裡是在每個執行緒內產生一個 Ramdom 物件
+            for (int i = 0; i < 100; i++)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    Random rm = new Random();
+                    int foo = rm.Next();
+                    Console.WriteLine(foo);
+                });
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine(new String('-', 80));
+
+            // 這裡使用同一個 Ramdom 物件
+            Random rmSingle = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    int foo = rmSingle.Next();
+                    Console.WriteLine(foo);
+                });
+            }
             Console.ReadLine();
         }
     }
